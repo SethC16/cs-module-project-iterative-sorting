17ef33d93db775f43bb2c25050a1d72722827d2e
Seth Cox <Sethcox16@yahoo.com>
2020-07-20 19:19:21 -0400

    Searching and iterative sorting completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,6 +7,10 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(cur_index+1, len(arr)):
+            if arr[j] < arr[smallest_index]: 
+                smallest_index = j
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
 
         # TO-DO: swap
@@ -18,7 +22,10 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    for i in range(0, (len(arr)-1)):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,28 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    for i in range(0, (len(arr))):
+        if arr[i] == target:
+            return i
+        
+    return -1 # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
+    last = len(arr) - 1
+    middle = 0
 
+    while first <= last:
+        middle = (last - first) // 2
+
+        if arr[middle] == target:
+            return middle
+        elif target > middle:
+            first = middle
+        else: 
+            last = middle
 
     return -1  # not found
